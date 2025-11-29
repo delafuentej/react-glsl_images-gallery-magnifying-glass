0d0b3def54c2889d5c3eb0adb423d676ba3ed1ab
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-11-29 15:42:11 +0100

    config. modifications for build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import glsl from "vite-plugin-glsl";
+import vitePreloadPlugin from "vite-plugin-preload";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), glsl()],
+  build: {
+    minify: "esbuild",
+    target: "esnext",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/react")) return "react";
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    vitePreloadPlugin({
+      rel: "preload",
+      as: "style",
+      includeCss: true,
+      includeJs: false,
+    }),
+    glsl({ include: ["**/*.glsl", "**/*.wgsl", "**/*.vert", "**/*.frag"] }),
+  ],
 });
